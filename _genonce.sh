8c71ef0167ad375605fde7da684d6e36c0fb2719
Stuart McGrigor <Stuart.McGrigor@midcentraldhb.govt.nz>
2023-06-16 13:21:02 +1200

    _genonce uses JQ betweeh SUSHI and IG-PUBLISHER to fixup generated IG

--- _genonce.sh
+++ _genonce.sh
@@ -16,14 +16,22 @@ echo "$txoption"
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
 
+#
+## We need to do a fixup after SUSHI and before IG-Publisher
+#
+sushi .
+
+jq '.definition.resource[70].exampleBoolean |= false' <fsh-generated/resources/ImplementationGuide-nz.central.ihub.ig.json >temp/fixedIg.json
+cp temp/fixedIg.json fsh-generated/resources/ImplementationGuide-nz.central.ihub.ig.json
+
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -jar $publisher -ig . $txoption $*
+	java -jar $publisher -ig . $txoption -no-sushi $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -jar $publisher -ig . $txoption $*
+		java -jar $publisher -ig . $txoption -no-sushi $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
